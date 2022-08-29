d53baa0b85e1a4122c2892040a0a3cc49e811a82
Zach Sanford <sanfordzachary@gmail.com>
2022-08-28 17:00:28 -0700

    2022-08-28 Daily Backup

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -2,25 +2,25 @@ namespace InstantInvoice.Models;
 
 public class Invoice
 {
-    public DateTime Date { get; set; }
-    public string Number { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+    public string Number { get; set; } = "0001";
     public string CompanyName { get; set; }
     public string CompanyAddress { get; set; }
     public string CompanyCity { get; set; }
     public string CompanyState { get; set; }
-    public int CompanyZipCode { get; set; }
+    public string CompanyZipCode { get; set; }
     public string? CompanyWebsite { get; set; }
     public string? CompanyEmail { get; set; }
     public string BillingName { get; set; }
     public string BillingAddress { get; set; }
     public string BillingCity { get; set; }
     public string BillingState { get; set; }
-    public int BillingZipCode { get; set; }
+    public string BillingZipCode { get; set; }
     public string? ShippingName { get; set; }
     public string? ShippingAddress { get; set; }
     public string? ShippingCity { get; set; }
     public string? ShippingState { get; set; }
-    public int? ShippingZipCode { get; set; }
+    public string? ShippingZipCode { get; set; }
     public List<LineItem> Items { get; set; }
     public double Subtotal => GetTotal(Items);
     public double Discount { get; set; }
