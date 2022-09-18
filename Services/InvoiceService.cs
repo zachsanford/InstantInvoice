8c4e4702ef445e2e780ddbf3633b4cdf52b58e32
Zach Sanford <sanfordzachary@gmail.com>
2022-09-18 07:31:35 -0700

    2022-09-18 Morning Backup

---
+++ Services/InvoiceService.cs
@@ -0,0 +1,10 @@
+using InstantInvoice.Models;
+namespace InstantInvoice.Services;
+
+public class InvoiceService
+{
+    public List<LineItem> GetFinalLineItemList(List<LineItem> _tempList)
+    {
+        
+    }
+}
