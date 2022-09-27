3b28d974aa69bfaf80f29d47af9fb9290191e5b6
Zach Sanford <sanfordzachary@gmail.com>
2022-09-27 13:05:10 -0700

    2022-09-27 Mid Day Backup

--- Services/InvoiceService.cs
+++ Services/InvoiceService.cs
@@ -3,8 +3,18 @@ namespace InstantInvoice.Services;
 
 public class InvoiceService
 {
-    public List<LineItem> GetFinalLineItemList(List<LineItem> _tempList)
+    public List<LineItem> ConvertListItem(LineItem[] lineItemArray)
     {
-        
+        List<LineItem> returnList = new List<LineItem>();
+
+        foreach (LineItem item in lineItemArray)
+        {
+            if (item.Quantity != 0 && !string.IsNullOrEmpty(item.Description) && item.UnitCost != 0.00)
+            {
+                returnList.Add(item);
+            }
+        }
+
+        return returnList;
     }
 }
